d9deddc5425ed6f4bfea4219dccf9acff6654078
doni010520 <donikasumii@gmail.com>
2025-11-07 17:29:10 -0300

    Update pdfGenerator.js

--- src/pdfGenerator.js
+++ src/pdfGenerator.js
@@ -8,11 +8,14 @@ async function getBrowser() {
   if (!browser) {
     browser = await puppeteer.launch({
       headless: 'new',
+      executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || '/usr/bin/chromium',
       args: [
         '--no-sandbox',
         '--disable-setuid-sandbox',
         '--disable-dev-shm-usage',
-        '--disable-gpu'
+        '--disable-gpu',
+        '--disable-software-rasterizer',
+        '--disable-extensions'
       ]
     });
   }
